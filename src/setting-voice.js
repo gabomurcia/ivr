7984773ed9ad180540f7b98c5651da5562c4a639
murcia <test@gmail.com>
2023-10-08 19:17:53 -0600

    .

--- src/setting-voice.js
+++ src/setting-voice.js
@@ -1,5 +1,5 @@
 module.exports = {
-	host: 'http://217.21.78.236:5000/call',  // cambiar la url nueva 
+	host: 'http://217.21.78.236:5000',  // cambiar la url nueva 
 	welcome: "Welcome to the ivr demo",
 	option: "Press 1 for Spanish. Press 2 for English",
 	notSelect: "Sorry, I didn't catch that. Please hang up and try again",
