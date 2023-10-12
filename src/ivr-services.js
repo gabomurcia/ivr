d3c5b89f7f254e83d805e6f328aa9f248f57916c
Esteban Guzman <esteban.d.guzman@gmail.com>
2023-10-11 22:42:20 -0500

    .

--- src/ivr-services.js
+++ src/ivr-services.js
@@ -1,107 +1,104 @@
-const plivo = require('plivo');
-const settingVoice = require('./setting-voice');
-const fs = require('fs');
-const translate = require('translate-google');
-const { format } = require('fecha');
-const path = require('path');
-
+const plivo = require("plivo");
+const settingVoice = require("./setting-voice");
+const fs = require("fs");
+const translate = require("translate-google");
+const { format } = require("fecha");
+const path = require("path");
 
 const IvrInicialize = (request, response) => {
 	const ivrStart = new plivo.Response();
-	const getInput = ivrStart.addGetInput(
-		{
-			'action': `${settingVoice.host}/switch-option`,
-			"method": 'POST',
-			'inputType': 'dtmf',
-			'digitEndTimeout': '5',
-			'language': 'en-US', // colocar el codigo del idioma
-			'redirect': 'true',
-			'finishOnKey': '#',
-			'log': 'true'
-		}
-	);
+	const getInput = ivrStart.addGetInput({
+		action: `${settingVoice.host}/switch-option`,
+		method: "POST",
+		inputType: "dtmf",
+		digitEndTimeout: "5",
+		language: "es-MX", // colocar el codigo del idioma
+		redirect: "true",
+		finishOnKey: "#",
+		log: "true",
+	});
 
-	getInput.addSpeak(`${settingVoice.welcome} ${settingVoice.option}`)
-	ivrStart.addSpeak(settingVoice.errorSelect)
-	console.log('----------------------------------------------')
-	response.set({ 'Content-Type': 'text/xml' })
-	response.end(ivrStart.toXML())
-}
+	getInput.addSpeak(`${settingVoice.welcome} ${settingVoice.option}`);
+	ivrStart.addSpeak(settingVoice.errorSelect);
+	console.log("----------------------------------------------");
+	response.set({ "Content-Type": "text/xml" });
+	response.end(ivrStart.toXML());
+};
 
 const TranslateVoice = async (request, response) => {
-	const voiceText = request.body.UnstableSpeech ?? ''
+	const voiceText = request.body.UnstableSpeech ?? "";
 	const ivrStart = new plivo.Response();
-	console.log('speech => ', request.body)
+	console.log("speech => ", request.body);
 
-	if(voiceText !== ''){
-		const timeZone = 'America/Merida';
-		const meridaDate = new Date().toLocaleString('en-US', { timeZone: timeZone });
-		const formattedDate = new Intl.DateTimeFormat('en-US', {
+	if (voiceText !== "") {
+		const timeZone = "America/Merida";
+		const meridaDate = new Date().toLocaleString("en-US", {
 			timeZone: timeZone,
-			weekday: 'long',
-			day: '2-digit',
-			month: '2-digit',
-			year: 'numeric',
-			hour: '2-digit',
-			minute: '2-digit',
-			second: '2-digit',
-			hour12: true
+		});
+		const formattedDate = new Intl.DateTimeFormat("en-US", {
+			timeZone: timeZone,
+			weekday: "long",
+			day: "2-digit",
+			month: "2-digit",
+			year: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+			second: "2-digit",
+			hour12: true,
 		}).format(new Date(meridaDate));
-	
-		const filePath = path.resolve(__dirname, `${formattedDate.replace(/[, :/]/g, ' ')}.txt`);
 
-		
-		console.log('frase original =>', voiceText)
-		const voiceTextTranslate = await translate(voiceText,{ to: 'es' })
-		console.log('frase traducida =>',voiceTextTranslate)
-		
-		fs.writeFileSync(filePath, voiceTextTranslate, 'utf-8');
+		const filePath = path.resolve(
+			__dirname,
+			`${formattedDate.replace(/[, :/]/g, " ")}.txt`
+		);
+
+		console.log("frase original =>", voiceText);
+		const voiceTextTranslate = await translate(voiceText, { to: "es" });
+		console.log("frase traducida =>", voiceTextTranslate);
+
+		fs.writeFileSync(filePath, voiceTextTranslate, "utf-8");
 	}
 
-	ivrStart.addSpeak(settingVoice.operationEnglish)
-	response.set({ 'Content-Type': 'text/xml' })
-	response.end(ivrStart.toXML())
-}
+	ivrStart.addSpeak(settingVoice.operationEnglish);
+	response.set({ "Content-Type": "text/xml" });
+	response.end(ivrStart.toXML());
+};
 
 const SwithOption = (request, response) => {
-	const { Digits } = request.body
-	const ivrStart = new plivo.Response()
-	const isOptionEnglish = '2'
-	const isOptionSpanish = '1' 
+	const { Digits } = request.body;
+	const ivrStart = new plivo.Response();
+	const isOptionEnglish = "2";
+	const isOptionSpanish = "1";
 
-	if(Digits === isOptionSpanish){
-		console.log('is option 1')
-		ivrStart.addSpeak(settingVoice.operationSpanish)
+	if (Digits === isOptionSpanish) {
+		console.log("is option 1");
+		ivrStart.addSpeak(settingVoice.operationSpanish);
 
-		response.set({ 'Content-Type': 'text/xml' })
+		response.set({ "Content-Type": "text/xml" });
 		response.end(ivrStart.toXML());
 
-		return
+		return;
 	}
 
-	if(Digits === isOptionEnglish ){
-		console.log('is option 2')
-		const getInput = ivrStart.addGetInput(
-			{
-				'action': `${settingVoice.host}/translate`,
-				'interimSpeechResultsCallback': `${settingVoice.host}/translate`,
-				'interimSpeechResultsCallbackMethod': 'POST',
-				'inputType': 'speech',
-				'language': 'en-US', // colocar el codigo del idioma
-				'redirect': 'true',
-				'speechEndTimeout': '10',
-				'log': 'true'
-			}
-		);
-	
-		getInput.addSpeak(settingVoice.operationEnglishStart)
-		ivrStart.addSpeak(settingVoice.operationEnglish)
-		console.log('----------------------------------------------')
-		response.set({ 'Content-Type': 'text/xml' })
-		response.end(ivrStart.toXML())
+	if (Digits === isOptionEnglish) {
+		console.log("is option 2");
+		const getInput = ivrStart.addGetInput({
+			action: `${settingVoice.host}/translate`,
+			interimSpeechResultsCallback: `${settingVoice.host}/translate`,
+			interimSpeechResultsCallbackMethod: "POST",
+			inputType: "speech",
+			language: "en-US", // colocar el codigo del idioma
+			redirect: "true",
+			speechEndTimeout: "10",
+			log: "true",
+		});
+
+		getInput.addSpeak(settingVoice.operationEnglishStart);
+		ivrStart.addSpeak(settingVoice.operationEnglish);
+		console.log("----------------------------------------------");
+		response.set({ "Content-Type": "text/xml" });
+		response.end(ivrStart.toXML());
 	}
+};
 
-}
-
-
-module.exports = {IvrInicialize, TranslateVoice , SwithOption}
+module.exports = { IvrInicialize, TranslateVoice, SwithOption };
--- src/setting-voice.js
+++ src/setting-voice.js
@@ -1,7 +1,7 @@
 module.exports = {
 	host: "http://217.21.78.236:5000", // cambiar la url nueva
 	welcome: "Welcome to the ivr demo",
-	option: "Preciona 1 para español y 2 para ingles",
+	option: "presione 1 para Español" , "press 2 for English",
 	notSelect: "Sorry, I didn't catch that. Please hang up and try again",
 	errorSelect: "Sorry, that's not a valid input",
 	operationEnglishStart: "please how can we help you",
