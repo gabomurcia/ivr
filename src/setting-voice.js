88d575814b69433e4a42674e45d9af6289100007
Esteban Guzman <esteban.d.guzman@gmail.com>
2023-10-11 22:37:50 -0500

    ajustes

--- src/setting-voice.js
+++ src/setting-voice.js
@@ -1,10 +1,10 @@
 module.exports = {
-	host: 'http://217.21.78.236:5000',  // cambiar la url nueva 
+	host: "http://217.21.78.236:5000", // cambiar la url nueva
 	welcome: "Welcome to the ivr demo",
-	option: "Press 1 for Spanish. Press 2 for English",
+	option: "Preciona 1 para español y 2 para ingles",
 	notSelect: "Sorry, I didn't catch that. Please hang up and try again",
 	errorSelect: "Sorry, that's not a valid input",
-	operationEnglishStart: 'please how can we help you',
-	operationEnglish: 'thank you, it will be answered shortly',
-	operationSpanish: 'Rediregiendo a operador en español'
-}
+	operationEnglishStart: "please how can we help you",
+	operationEnglish: "thank you, it will be answered shortly",
+	operationSpanish: "Rediregiendo a operador en español",
+};
